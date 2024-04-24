8096a3f41def846a146070671f4403b8f5eaeb53
Shin Soomin <133620313+nimoos9480@users.noreply.github.com>
2024-04-24 09:54:20 +0900

    Feat_EditFrom 컴포넌트로 유효성 검사

---
+++ Udemy/BlazorApp/Models/ExampleModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Models
+{
+    public class ExampleModel
+    {
+        [Required(ErrorMessage ="이름은 필수입니다.")] // 반드시 입력
+        [StringLength(10, ErrorMessage ="이름을 10자 이하로 입력하세요.")] // 문자열길이 설정, 초과시 에러메세지
+        public string Name { get; set; }
+    }
+}
